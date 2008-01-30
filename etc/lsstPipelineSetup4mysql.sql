10171df467238b667f0672be671f7223b241c091
smm <smm@git.lsstcorp.org>
2008-01-30 22:20:58 +0000

    update pipeline sql to match new schema

--- etc/lsstPipelineSetup4mysql.sql
+++ etc/lsstPipelineSetup4mysql.sql
@@ -10,7 +10,7 @@
 CREATE TABLE DIASourceTemplate LIKE DIASource;
 
 ALTER TABLE DIASourceTemplate
-    DROP KEY exposureId,
+    DROP KEY ccdExposureId,
     DROP KEY filterId,
     DROP KEY movingObjectId,
     DROP KEY objectId,
